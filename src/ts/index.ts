f78a834790b3ac663a54f6f1f88b7e250e16874b
abdo1rahman <abdoasem2055@gmail.com>
2025-06-25 16:02:34 +0300

    Final touches remain

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,26 @@
 import { animate } from "motion";
+
+async function getAdvice(): Promise<string> {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const json = await res.json();
+  return json.slip.advice;
+}
+
+const adviceElement = document.getElementById("advice");
+
+window.addEventListener("load", async () => {
+  const advice = await getAdvice();
+  const adviceElement = document.getElementById("advice");
+  if (adviceElement) {
+    adviceElement.textContent = advice;
+  }
+});
+
+
+// Example inside an event listener
+document.getElementById("btn")?.addEventListener("click", async () => {
+  const advice = await getAdvice();
+  if (adviceElement) {
+    adviceElement.textContent = advice;
+  }
+});
